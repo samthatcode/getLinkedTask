190252956f63927a5e362c491375086b7d600481
samthatcode <samthatcode@gmail.com>
2023-09-21 17:42:24 +0100

    Bug fix for UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,41 +5,84 @@ import behindHeroMan from "../assets/images/behindHeroMan.png";
 import pinkUnder from "../assets/images/pinkUnder.png";
 import fire from "../assets/images/fire.png";
 import chain from "../assets/images/chain.png";
+import bulb from "../assets/images/bulb.png";
 
 const Hero = () => {
   return (
     <div className="px-8 border-b border-gray-400 relative bg-transparent">
       <div className="flex justify-center md:justify-end my-4">
-        <h1 className="capitalize text-white italic text-xl font-bold">
+        <h1 className="capitalize text-white italic text-sm md:text-xl font-bold">
           igniting a revolution in HR innovation
         </h1>
         <img src={pinkUnder} alt="/" className="w-36 my-4 absolute top-3" />
       </div>
 
-      <div className="flex flex-col md:flex-row">
-        <div className="md:w-1/2 md:p-8 text-white">
-          <div className="flex justify-center items-center">
+      <div className="flex flex-col md:flex-row md:text-left text-center">
+        <div className="md:w-1/2 md:p-14 text-white">
+          <img
+            src={bulb}
+            alt="/"
+            className="md:w-10 md:h-10 md:ml-56 md:block hidden"
+          />
+
+          <div className="md:flex justify-center items-center hidden">
             <h1 className="md:text-4xl text-lg font-bold">
               getlinked Tech Hackathon <span className="text-purple">1.0</span>
             </h1>
-            <span>
-              <img src={chain} alt="/" className="w-10 h-10" />
-            </span>
-            <span>
-              <img src={fire} alt="/" className="w-10 h-10 ml-2" />
-            </span>
+            <div className="flex justify-center items-center mt-14 md:mt-10 md:mr-40 mr-40">
+              <div>
+                <img src={chain} alt="/" className="md:w-20 md:h-10 w-10 h-5" />
+              </div>
+              <div>
+                <img
+                  src={fire}
+                  alt="/"
+                  className="md:w-20 md:h-10 w-10 h-5 ml-2"
+                />
+              </div>
+            </div>
           </div>
+
+          <div className="relative flex justify-center items-center mt-16 md:hidden">
+          <img src={bulb} alt="/" className="w-5 h-5 top-[-18px] right-[173px] md:hidden absolute" />
+            <h1 className="md:text-4xl text-xl font-bold">
+              getlinked Tech Hackathon <span className="text-purple">1.0</span>
+            </h1>
+            <div className="flex justify-center items-center mt-5 mr-20">
+              <div>
+                <img src={chain} alt="/" className=" w-10 h-5" />
+              </div>
+              <div>
+                <img src={fire} alt="/" className=" w-10 h-5 ml-2" />
+              </div>
+            </div>
+          </div>
+
           <p className="my-5">
             Participate in getlinked tech Hackathon 2023 stand a chance to win a
             Big prize
           </p>
           <Link
             to="/register"
-            className="inline-block text-sm text-white primary py-2 px-4 rounded"
+            className="inline-block text-sm text-white primary py-2 px-7 rounded"
           >
             Register
           </Link>
-          <div className="flex space-x-2 text-lg my-12">
+          <div className="space-x-2 text-lg my-12 hidden md:flex">
+            <div className="text-white p-1 rounded">
+              <span className="text-3xl">00</span>
+              <span className="text-[10px]">H</span>
+            </div>
+            <div className="text-white p-1 rounded">
+              <span className="text-3xl">00</span>
+              <span className="text-[10px]">M</span>
+            </div>
+            <div className="text-white p-1 rounded">
+              <span className="text-3xl">00</span>
+              <span className="text-[10px]">S</span>
+            </div>
+          </div>
+          <div className="space-x-2 text-lg my-12 md:hidden flex ml-20">
             <div className="text-white p-1 rounded">
               <span className="text-3xl">00</span>
               <span className="text-[10px]">H</span>
@@ -55,7 +98,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="md:w-1/2 relative">        
+        <div className="md:w-1/2 relative">
           <img src={heroMan} alt="/" className="w-full" />
           <img
             src={behindHeroMan}
